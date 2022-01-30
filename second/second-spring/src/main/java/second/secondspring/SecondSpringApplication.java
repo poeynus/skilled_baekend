ad9466860759a8007125a586da6e3ed40eb58689
poeynus <poeynus@gmail.com>
2022-01-30 21:40:18 +0900

    second project

--- second/second-spring/src/main/java/second/secondspring/SecondSpringApplication.java
+++ second/second-spring/src/main/java/second/secondspring/SecondSpringApplication.java
@@ -1,13 +1,13 @@
-package com.cos.blog;
+package second.secondspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApplication {
+public class SecondSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
+		SpringApplication.run(SecondSpringApplication.class, args);
 	}
 
 }
